1eac05d41c8e03e45dae53e6d2b2cd6f091b5c30
Acediatic <hello@adamsinclair.kiwi>
2022-09-04 12:03:44 +1200

    BREAKING: moves thresholds to new class

---
+++ lib/rep_counting/thresholds/squat_thresholds.dart
@@ -0,0 +1,63 @@
+import 'package:google_ml_kit_example/rep_counting/movement_phase.dart';
+import 'package:google_ml_kit_example/rep_counting/thresholds/thresholds.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+
+class SquatThresholds extends Thresholds {
+  /// Internal angle of the hip, knee, and ankle
+  static const num _lowerThreshold = 1.91986; // (180 - 70) * (pi / 180);
+  static const num _upperThreshold = 2.61799; // (180-30) * (pi / 180);
+
+  @override
+  MovementPhase getMovementPhase(Pose pose) {
+    List<num> kneeAngles = getAngles(pose);
+    num leftKneeAngle = kneeAngles[0];
+    num rightKneeAngle = kneeAngles[1];
+
+    if (leftKneeAngle > _upperThreshold && rightKneeAngle > _upperThreshold) {
+      // top
+      return MovementPhase.top;
+    } else if (leftKneeAngle < _lowerThreshold &&
+        rightKneeAngle < _lowerThreshold) {
+      // bottom
+      return MovementPhase.bottom;
+    } else {
+      // intermediate
+      return MovementPhase.intermediate;
+    }
+  }
+
+  @override
+  List<double> getAngles(Pose pose) {
+    if (pose.landmarks.isEmpty) {
+      throw Exception('No pose detected');
+    }
+    try {
+      PoseLandmark leftKnee =
+          pose.landmarks[PoseLandmarkType.leftKnee] as PoseLandmark;
+      PoseLandmark rightKnee =
+          pose.landmarks[PoseLandmarkType.rightKnee] as PoseLandmark;
+      PoseLandmark leftAnkle =
+          pose.landmarks[PoseLandmarkType.leftAnkle] as PoseLandmark;
+      PoseLandmark rightAnkle =
+          pose.landmarks[PoseLandmarkType.rightAnkle] as PoseLandmark;
+      PoseLandmark leftHip =
+          pose.landmarks[PoseLandmarkType.leftHip] as PoseLandmark;
+      PoseLandmark rightHip =
+          pose.landmarks[PoseLandmarkType.rightHip] as PoseLandmark;
+
+      if (!confidenceCheck(
+          [leftKnee, rightKnee, leftAnkle, rightAnkle, leftHip, rightHip])) {
+        throw StateError('Pose confidence too low');
+      }
+      // Calculate the angle between the left hip, left knee, and left ankle
+      double leftHipKneeAnkleAngle = lawOfCosines(leftKnee, leftHip, leftAnkle);
+
+      // Calculate the angle between the right hip, right knee, and right ankle
+      double rightHipKneeAnkleAngle =
+          lawOfCosines(rightKnee, rightHip, rightAnkle);
+      return [leftHipKneeAnkleAngle, rightHipKneeAnkleAngle];
+    } on TypeError {
+      throw StateError('Insufficient pose information');
+    }
+  }
+}
--- lib/rep_counting/thresholds/thresholds.dart
+++ lib/rep_counting/thresholds/thresholds.dart
@@ -4,72 +4,19 @@ import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
 
 import '../movement_phase.dart';
 
-class Thresholds {
-  /// Internal angle of the hip, knee, and ankle
-  static const num _lowerThreshold = 1.91986; // (180 - 70) * (pi / 180);
-  static const num _upperThreshold = 2.61799; // (180-30) * (pi / 180);
-
+abstract class Thresholds {
   static const double _minConfidence = 0.5;
 
   /// Returns the movement phase of the current frame based on the detections.
-  static MovementPhase thresholdSquat(Pose pose) {
-    List<num> kneeAngles = _squatAngles(pose);
-    num leftKneeAngle = kneeAngles[0];
-    num rightKneeAngle = kneeAngles[1];
-
-    if (leftKneeAngle > _upperThreshold && rightKneeAngle > _upperThreshold) {
-      // top
-      return MovementPhase.top;
-    } else if (leftKneeAngle < _lowerThreshold &&
-        rightKneeAngle < _lowerThreshold) {
-      // bottom
-      return MovementPhase.bottom;
-    } else {
-      // intermediate
-      return MovementPhase.intermediate;
-    }
-  }
+  MovementPhase getMovementPhase(Pose pose);
 
   /// Returns the angles of the left and right knees,
   /// in the format [leftKneeAngle, rightKneeAngle].
   /// Throws an error if the pose is invalid.
-  static List<double> _squatAngles(Pose pose) {
-    if (pose.landmarks.isEmpty) {
-      throw Exception('No pose detected');
-    }
-    try {
-      PoseLandmark leftKnee =
-          pose.landmarks[PoseLandmarkType.leftKnee] as PoseLandmark;
-      PoseLandmark rightKnee =
-          pose.landmarks[PoseLandmarkType.rightKnee] as PoseLandmark;
-      PoseLandmark leftAnkle =
-          pose.landmarks[PoseLandmarkType.leftAnkle] as PoseLandmark;
-      PoseLandmark rightAnkle =
-          pose.landmarks[PoseLandmarkType.rightAnkle] as PoseLandmark;
-      PoseLandmark leftHip =
-          pose.landmarks[PoseLandmarkType.leftHip] as PoseLandmark;
-      PoseLandmark rightHip =
-          pose.landmarks[PoseLandmarkType.rightHip] as PoseLandmark;
-
-      if (!_confidenceCheck(
-          [leftKnee, rightKnee, leftAnkle, rightAnkle, leftHip, rightHip])) {
-        throw StateError('Pose confidence too low');
-      }
-      // Calculate the angle between the left hip, left knee, and left ankle
-      double leftHipKneeAnkleAngle =
-          _lawOfCosines(leftKnee, leftHip, leftAnkle);
-
-      // Calculate the angle between the right hip, right knee, and right ankle
-      double rightHipKneeAnkleAngle =
-          _lawOfCosines(rightKnee, rightHip, rightAnkle);
-      return [leftHipKneeAnkleAngle, rightHipKneeAnkleAngle];
-    } on TypeError {
-      throw StateError('Insufficient pose information');
-    }
-  }
+  List<double> getAngles(Pose pose);
 
   /// Checks the confidence is above the [_minConfidence] threshold
-  static bool _confidenceCheck(List<PoseLandmark> landmarks) {
+  bool confidenceCheck(List<PoseLandmark> landmarks) {
     for (PoseLandmark landmark in landmarks) {
       if (landmark.likelihood < _minConfidence) {
         return false;
@@ -80,7 +27,7 @@ class Thresholds {
 
   /// Calculates the angle between three points,
   /// as per https://stackoverflow.com/a/1211243/16521305
-  static double _lawOfCosines(
+  double lawOfCosines(
       PoseLandmark middlePose, PoseLandmark endPose, PoseLandmark endPose2) {
     double p12 = _distanceBetweenPoints(
         middlePose.x, middlePose.y, endPose.x, endPose.y);
