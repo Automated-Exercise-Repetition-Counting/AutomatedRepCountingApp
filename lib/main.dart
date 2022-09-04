b61af0ba376a6cfc5f48bb829db9dfac5a65035d
Acediatic <hello@adamsinclair.kiwi>
2022-09-04 14:44:51 +1200

    feat: updates homescreen to allow exercise selection

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:google_ml_kit_example/automatic_rep_counter/automatic_rep_counter.dart';
 import 'package:google_ml_kit_example/automatic_rep_counter/exercise/exercise.dart';
 import 'package:google_ml_kit_example/automatic_rep_counter/exercise/exercises/pull_up_exercise.dart';
+import 'package:google_ml_kit_example/automatic_rep_counter/exercise/exercises/push_up_exercise.dart';
+import 'package:google_ml_kit_example/automatic_rep_counter/exercise/exercises/squat_exercise.dart';
 import 'package:google_ml_kit_example/vision_detector_views/pose_detector_view.dart';
 
 List<CameraDescription> cameras = [];
@@ -21,31 +23,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(
-        exercise: PullUpExercise(),
-      ),
+      home: Home(),
     );
   }
 }
 
 class Home extends StatefulWidget {
-  final Exercise exercise;
-  Home({required this.exercise});
+  Home();
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  late final AutomaticRepCounter _repCounter;
+  late AutomaticRepCounter _repCounter;
 
   @override
   void initState() {
     super.initState();
-    _repCounter = AutomaticRepCounter(exercise: widget.exercise);
+  }
+
+  void initRepCounter(Exercise exercise) {
+    _repCounter = AutomaticRepCounter(exercise: exercise);
     _repCounter.addListener(() {
       setState(() {});
     });
+  }
+
+  void startNewActivity(Exercise exercise) {
+    initRepCounter(exercise);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => PoseDetectorView(
+          repCounter: _repCounter,
+        ),
+      ),
+    );
   }
 
   @override
@@ -58,59 +72,21 @@ class _HomeState extends State<Home> {
       ),
       body: SafeArea(
         child: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  ExpansionTile(
-                    title: const Text('Vision APIs'),
-                    children: [
-                      CustomCard('Pose Detection',
-                          PoseDetectorView(repCounter: _repCounter)),
-                    ],
-                  ),
-                  // Gets information from the repcounter
-                  Text("Reps: ${_repCounter.reps}"),
-                  Text("Phase: ${_repCounter.phase}"),
-                ],
-              ),
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () => startNewActivity(SquatExercise()),
+                  child: Text("Squats")),
+              ElevatedButton(
+                  onPressed: () => startNewActivity(PullUpExercise()),
+                  child: Text("Pull Ups")),
+              ElevatedButton(
+                  onPressed: () => startNewActivity(PushUpExercise()),
+                  child: Text("Push Ups")),
+            ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-class CustomCard extends StatelessWidget {
-  final String _label;
-  final Widget _viewPage;
-  final bool featureCompleted;
-
-  const CustomCard(this._label, this._viewPage, {this.featureCompleted = true});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      margin: EdgeInsets.only(bottom: 10),
-      child: ListTile(
-        tileColor: Theme.of(context).primaryColor,
-        title: Text(
-          _label,
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        onTap: () {
-          if (!featureCompleted) {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content:
-                    const Text('This feature has not been implemented yet')));
-          } else {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => _viewPage));
-          }
-        },
       ),
     );
   }
