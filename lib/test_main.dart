4b6ee5f5b109b2a5d835916e092bfe7b74d1c0c0
Acediatic <hello@adamsinclair.kiwi>
2022-09-19 21:58:54 +1200

    feat: switches to push ups testing

--- lib/test_main.dart
+++ lib/test_main.dart
@@ -12,66 +12,70 @@ import 'package:puioio/automatic_rep_counter/optical_flow/optical_flow_calculato
 
 final PoseDetector _poseDetector = PoseDetector(options: PoseDetectorOptions());
 
-List<String> squatVideos = [
-  // 'Squat1/',
-  // 'Squat10/',
-  // 'Squat11/',
-  // 'Squat12/',
-  // 'Squat13/',
-  // 'Squat14/',
-  // 'Squat15/',
-  // 'Squat16/',
-  // 'Squat17/',
-  // 'Squat18/',
-  // 'Squat19/',
-  // 'Squat20/',
-  // 'Squat21/',
-  // 'Squat22/',
-  // 'Squat23/',
-  // 'Squat24/',
-  // 'Squat25/',
-  // 'Squat26/',
-  // 'Squat27/',
-  // 'Squat28/',
-  // 'Squat29/',
-  // 'Squat3/',
-  // 'Squat30/',
-  // 'Squat4/',
-  'Squat5/',
-  'Squat7/',
-  'Squat8/',
-  'Squat9/',
+List<String> videos = [
+  'Pushup1/',
+  'Pushup10/',
+  'Pushup11/',
+  'Pushup12/',
+  'Pushup13/',
+  'Pushup14/',
+  'Pushup15/',
+  'Pushup16/',
+  'Pushup17/',
+  'Pushup18/',
+  'Pushup19/',
+  'Pushup2/',
+  'Pushup20/',
+  'Pushup21/',
+  'Pushup22/',
+  'Pushup23/',
+  'Pushup24/',
+  'Pushup25/',
+  'Pushup26/',
+  'Pushup27/',
+  'Pushup28/',
+  'Pushup29/',
+  'Pushup3/',
+  'Pushup30/',
+  'Pushup4/',
+  'Pushup5/',
+  'Pushup6/',
+  'Pushup7/',
+  'Pushup8/',
+  'Pushup9/',
 ];
 
-List<int> squatFrames = [
-  // 418,
-  // 93,
-  // 302,
-  // 219,
-  // 445,
-  // 480,
-  // 192,
-  // 314,
-  // 115,
-  // 697,
-  // 1884,
-  // 134,
-  // 851,
-  // 2147,
-  // 447,
-  // 215,
-  // 716,
-  // 1335,
-  // 945,
-  // 28,
-  // 293,
-  // 365,
-  // 711,
-  // 217,
-  416,
-  365,
-  382,
-  696,
+List<int> frames = [
+  2187,
+  884,
+  355,
+  2477,
+  106,
+  471,
+  138,
+  864,
+  280,
+  288,
+  744,
+  370,
+  408,
+  725,
+  252,
+  867,
+  327,
+  529,
+  859,
+  854,
+  6621,
+  462,
+  399,
+  129,
+  360,
+  378,
+  522,
+  743,
+  450,
+  401,
 ];
 // Future<void> main() async {
 //   WidgetsFlutterBinding.ensureInitialized();
@@ -91,14 +95,11 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final directory = await getApplicationDocumentsDirectory();
 
-  String pathPrefix = "assets/test/Squat/";
-
-  final videos = squatVideos;
-  final numFrames = squatFrames;
+  String pathPrefix = "assets/test/PushUps/";
 
   for (int i = 0; i < videos.length; i++) {
     print("FOR VIDEO: " + videos[i]);
-    await testVideo(pathPrefix + videos[i], numFrames[i], directory);
+    await testVideo(pathPrefix + videos[i], frames[i], directory);
   }
 }
 
